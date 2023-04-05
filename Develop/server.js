d3d470e1bf362a22d422842681b1e09e1a3dce39
John Shirey <you@example.com>
2023-04-04 22:05:02 -0400

    updated main server.js/route folders

--- Develop/server.js
+++ Develop/server.js
@@ -1,14 +1,23 @@
+// Requiring needed elements
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const apiRoutes = require('apiRoutes');
+const htmlRoutes = require('htmlRoutes');
 
+// Variables for the use of the port and the app
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// Use of host public folder, JSON and Parse URL Encoded
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true }));
 
-app.get('/', (req, res) =>
-    res.sendFile(path.join (__dirname, '/public/index.html'))
-);
+// Use of the routes
+app.use('/api, apiRoutes');
+app.use('/html, htmlRoutes');
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
